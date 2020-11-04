29e286d7bc0db83121fdc698c18cacdc9f685408
AntoBrandi <antonio.brandi@outlook.it>
2020-11-04 08:41:53 +0000

    added description package in lesson 5

---
+++ L4-ROS-with-Arduino-and-Raspberry-Pi/arduinobot_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/arduinobot-tutorial/L4 - ROS with Arduino and Raspberry Pi/arduinobot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/arduinobot-tutorial/L3 - What is ROS/arduinobot_ws/devel;/home/ubuntu/DextroBot/ROS/dextrobot_ws/devel;/home/ubuntu/arduinobot/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/arduinobot-tutorial/L3 - What is ROS/arduinobot_ws/devel;/home/ubuntu/DextroBot/ROS/dextrobot_ws/devel;/home/ubuntu/arduinobot/devel;/opt/ros/melodic' != '' else []
---
+++ L5-Digital-Twin-with-URDF/arduinobot_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/arduinobot-tutorial/L5-Digital-Twin-with-URDF/arduinobot_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/opt/vc/lib:/opt/vc/lib'
+export PWD='/home/ubuntu/arduinobot-tutorial/L5-Digital-Twin-with-URDF/arduinobot_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/arduinobot-tutorial/L5-Digital-Twin-with-URDF/arduinobot_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ubuntu/arduinobot-tutorial/L5-Digital-Twin-with-URDF/arduinobot_ws/src:$ROS_PACKAGE_PATH"
