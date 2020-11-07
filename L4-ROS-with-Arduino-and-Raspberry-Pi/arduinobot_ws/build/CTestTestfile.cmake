b89f8e6ff55ce4fa9a957a6c83404d9ea2809da2
AntoBrandi <antonio.brandi@outlook.it>
2020-11-07 15:48:35 +0000

    removed useless build file

--- L4-ROS-with-Arduino-and-Raspberry-Pi/arduinobot_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/arduinobot-tutorial/L4 - ROS with Arduino and Raspberry Pi/arduinobot_ws/src
-# Build directory: /home/ubuntu/arduinobot-tutorial/L4 - ROS with Arduino and Raspberry Pi/arduinobot_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("arduinobot_test")
