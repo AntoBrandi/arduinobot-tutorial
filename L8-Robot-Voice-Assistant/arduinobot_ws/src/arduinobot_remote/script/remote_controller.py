7974d849d11a3425d9f2bb646d079d6f1ddd47c3
AntoBrandi <antonio.brandi@outlook.it>
2020-11-08 18:14:48 +0000

    added jarvis coice subscriber

--- L8-Robot-Voice-Assistant/arduinobot_ws/src/arduinobot_remote/script/remote_controller.py
+++ L8-Robot-Voice-Assistant/arduinobot_ws/src/arduinobot_remote/script/remote_controller.py
@@ -2,6 +2,7 @@
 import rospy
 from sensor_msgs.msg import JointState
 from std_msgs.msg import String
+from pick_task import GoalPublisher
 
 # function that gets called every time a new message is published on the topic /jarvis_controller
 def jarvis_controller_callback(data):
@@ -10,6 +11,11 @@ def jarvis_controller_callback(data):
 def jarvis_messenger_callback(data):
     rospy.loginfo('Remote message received: %s', data.data)  
 
+def jarvis_voice_callback(data):
+    gp = GoalPublisher()
+    if data.data:
+        gp.setRobotJoint()
+
 if __name__ == '__main__':
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
     rospy.init_node('remote_controller', anonymous=True)
@@ -17,6 +23,7 @@ if __name__ == '__main__':
     # Subscribe to the topic in which the voice assistant is writing
     rospy.Subscriber("jarvis_controller", JointState, jarvis_controller_callback)
     rospy.Subscriber("jarvis_messenger", String, jarvis_messenger_callback)
+    rospy.Subscriber("jarvis_voice", Bool, jarvis_voice_callback)
 
     # Keep the subscriber running
     rospy.spin()
