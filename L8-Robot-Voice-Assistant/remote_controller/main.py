0bce6be07955eba17d66dac8fc3232f3966b08bd
anton <antonio.brandi@outlook.it>
2020-11-08 19:02:08 +0100

    added voice control features

--- L8-Robot-Voice-Assistant/remote_controller/main.py
+++ L8-Robot-Voice-Assistant/remote_controller/main.py
@@ -1,6 +1,7 @@
 from ros_interface import RosInterface
 from messenger import Messenger
 from controller import Controller
+from voice_assistant import Assistant
 
 ros = RosInterface()
 
@@ -11,6 +12,7 @@ if __name__ == "__main__":
         print("What would you like to do with your robot?")
         print("1 for send a message to the robot")
         print("2 for send joint positions")
+        print("3 for the voice assistant")
         user_input = input()
         try:
             selected_task = int(user_input)
@@ -18,6 +20,8 @@ if __name__ == "__main__":
                 Messenger(ros)
             elif selected_task == 2:
                 Controller(ros)
+            elif selected_task == 3:
+                Assistant(ros)
             else:
                 print("Invalid Input")
         except ValueError:
--- L8-Robot-Voice-Assistant/remote_controller/ros_interface.py
+++ L8-Robot-Voice-Assistant/remote_controller/ros_interface.py
@@ -1,6 +1,7 @@
 import roslibpy
 import messenger
 import controller
+import voice_assistant
 
 ROBOT_IP = "192.168.0.166"
 ROBOT_PORT = 9090
@@ -12,12 +13,14 @@ class RosInterface:
         self.client = roslibpy.Ros(host=ROBOT_IP, port=ROBOT_PORT)
         self.messenger_publisher = roslibpy.Topic(self.client, messenger.MESSENGER_TOPIC, messenger.MESSENGER_TYPE)
         self.controller_publisher = roslibpy.Topic(self.client, controller.CONTROLLER_TOPIC, controller.CONTROLLER_TYPE)
+        self.voice_publisher = roslibpy.Topic(self.client, voice_assistant.VOICE_TOPIC, voice_assistant.VOICE_TYPE)
 
     def connect(self):
         try:
             self.client.run()
             self.messenger_publisher.advertise()
             self.controller_publisher.advertise()
+            self.voice_publisher.advertise()
             print('Connected')
         except Exception as e:
             print('WebSocket Error: ' + str(e))
@@ -28,6 +31,8 @@ class RosInterface:
                 self.messenger_publisher.publish(msg)
             elif topic == controller.CONTROLLER_TOPIC:
                 self.controller_publisher.publish(msg)
+            elif topic == voice_assistant.VOICE_TOPIC:
+                self.voice_publisher.publish(msg)
             else:
                 print('Invalid topic')
             print('Sending message...')
