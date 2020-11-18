9fc13454d0d5503cb3a91f207de77d5dfd781945
anton <antonio.brandi@outlook.it>
2020-11-18 11:50:41 +0100

    added documentation

---
+++ L8-Remote-Control/arduinobot_ws/src/arduinobot_remote/script/remote_controller.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import JointState
+from std_msgs.msg import String
+
+"""
+  arduinobot - remote_controller
+
+  This script implements an interface between the robot and the external world.
+  It is in charge of receive messages from the external world, eventually validate them,
+  and publish those on the correct topic of the robot where those will be used by other modules
+
+  The open interface is listening on topics:
+
+  - jarvis_messenger : Simple String messages are published. It may be useful 
+                       for sending command to the robot via messages coming from different sources
+                       (es. Telegram actuated robot)
+
+  - jarvis_controller : JointState message are published. It receives desidered poses of each joint 
+                        of the robot and executes them
+  
+
+  Copyright (c) 2020 Antonio Brandi.  All right reserved.
+"""
+
+
+def jarvis_controller_callback(data):
+    # function that gets called every time a new message is published on the topic /jarvis_controller
+    # it receives a Joint state message and forwards it to the joint_states topic
+    pub.publish(data) 
+
+def jarvis_messenger_callback(data):
+    # function that gets called every time a new message is published on the topic /jarvis_messenger
+    rospy.loginfo('Remote message received: %s', data.data)  
+
+
+if __name__ == '__main__':
+    # Inizialize a ROS node called remote_controller
+    rospy.init_node('remote_controller', anonymous=True)
+
+    # register a publisher on the topic /joint_states that will publish JointState messages
+    pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+
+    # register a subscriber on the topics /jarvis_messenger that will listen for String messages
+    # and the topic /jarvis_controller that will receive JointState messages
+    # when a new message is received, the callback function is triggered and starts its execution
+    rospy.Subscriber("jarvis_controller", JointState, jarvis_controller_callback)
+    rospy.Subscriber("jarvis_messenger", String, jarvis_messenger_callback)
+
+    # Keep the subscriber running
+    rospy.spin()
